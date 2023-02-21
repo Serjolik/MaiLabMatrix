3e969c2d33b1c25b58178b4dc8c612b2d9bf1b34
Sergey <Seregsa2014@gmail.com>
2023-02-21 22:03:47 +0300

    Initial commit

---
+++ MaiLabMatrix/Matrix.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+public class Matrix
+{
+	private List<List<float>> matrix = new List<List<float>>();
+
+	public Matrix(List<List<float>> matrix)
+	{
+		this.matrix = matrix;
+	}
+
+	public List<float> Line(int index)
+	{	// returning needed lane
+		return matrix[index];
+	}
+
+	public List<float> LineMultiplication(List<float> line, float multiplier)
+	{	// Returned new line with multiplier
+		var newLine = new List<float>();
+
+		foreach(var number in line)
+		{
+			newLine.Add(number * multiplier);
+		}
+
+		return (newLine);
+    }
+
+	public void SwapLines(int firstIndex, int secondIndex)
+	{	// Swap lines in our matrix
+		var bufferLine = matrix[firstIndex];
+		matrix[firstIndex] = matrix[secondIndex];
+		matrix[secondIndex] = bufferLine;
+	}
+
+	public void LineSubtraction(List<float> line, int lineNumber)
+    {   // Subtracts a line from the selected line
+		int index = 0;
+		foreach (float element in matrix[lineNumber])
+		{
+			line[index] -= element;
+			line[index] *= -1;
+			index++;
+		}
+		matrix[lineNumber] = new List<float>(line);
+    }
+
+	public void PrintResult()
+	{
+		foreach(List<float> line in matrix)
+		{
+			foreach(float element in line)
+			{
+				Console.WriteLine(element);
+			}
+			Console.WriteLine();
+		}
+	}
+
+}
---
+++ MaiLabMatrix/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Matrix clMatrix;
+
+        List<List<float>> matrix = new List<List<float>>()
+        {
+        new List<float> { 1, 1, 1 },
+        new List<float> { 2, 2, 2 },
+        new List<float> { 3, 3, 3 },
+        };
+
+        clMatrix = new Matrix(matrix);
+
+        clMatrix.SwapLines(1, 2);
+
+        clMatrix.LineSubtraction(clMatrix.LineMultiplication(clMatrix.Line(0), 5), 1);
+
+        clMatrix.PrintResult();
+
+        Console.ReadKey();
+    }
+}
